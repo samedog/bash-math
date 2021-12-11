22867a15d5bb31d6c684ae9528ed9a44fbca9566
samedog <the.samedog@gmail.com>
2021-12-10 22:38:24 -0300

    rewrote division and fixed some issues

--- math.sh
+++ math.sh
@@ -30,65 +30,8 @@ function fpow(){
     echo $cnt
 }
 
-function process_floats(){
-
-loop=1
-    for i in "$@"
-    do
-        ## if any is a negative number
-        if [[ $i == *"-"* ]];then
-            declare int${loop}_n=1
-        fi
-        ##pre-processing the first number
-        if [[ "$i" == *","* ]];then
-            declare float${loop}_int=$(echo $i | cut -d',' -f1)
-            declare float${loop}_dec=$(echo $i | cut -d',' -f2)
-        elif [[ "$i" == *"."* ]];then
-            declare float${loop}_int=$(echo $i | cut -d'.' -f1)
-            declare float${loop}_dec=$(echo $i | cut -d'.' -f2)
-        else
-            declare float${loop}_int=$i
-            declare float${loop}_dec=0
-        fi
-        
-         (( loop+=1 ))
-    done
-    
-    #stop the loop to count de decimal parts on each number
-    #and single number operations
-    DECIMALS=${#float1_dec}
-    if [[ ${#float1_dec} -le ${#float2_dec} ]];then
-        DECIMALS=${#float2_dec}
-    fi
-    #for easier processing we join the int and dec parts and 
-    #fill any necessary spots with zeroes
-    if [[ ${#float1_dec} -lt ${#float2_dec} ]];then
-        num_to_add=$(( ${#float2_dec} - ${#float1_dec} ))
-        for (( i=0; i<$num_to_add; i++ ))
-        do
-            float1_dec="$float1_dec"0
-        done
-    fi
-    if [[ ${#float2_dec} -lt ${#float1_dec} ]];then
-        num_to_add=$(( ${#float1_dec} - ${#float2_dec} ))
-        for (( i=0; i<$num_to_add; i++ ))
-        do
-            float2_dec="$float2_dec"0
-        done
-        
-    fi
-    ##decimals are normalized, let's join the numbers into 1
-    whole_num_1=$float1_int$float1_dec
-    whole_num_2=$float2_int$float2_dec
-
-    echo "$whole_num_1|$whole_num_2|$int1_n|$int2_n|$DECIMALS"
-}
-
-
-# this is hacky as hell but it works
 # can add non floats and will return a float
-# add or substract 2 floats
-# if a single int is given it will return a float
+# add or substract
 
 function sum_float(){
     DECIMALS=1
@@ -155,10 +98,10 @@ function mult_float(){
         if [[ ! $i == *","* ]];then
             i=$i,0
         fi
-        integer="${i%%","*}"
+        number=${i//,}
         decimal="${i#*","}"
         (( DECIMALS += ${#decimal} ))
-        (( pre_result *= $integer$decimal ))
+        (( pre_result *= $number ))
         (( count++ ))
     done
     DECIMALS=$(( ${#pre_result} - $DECIMALS ))
@@ -182,64 +125,72 @@ function div_float(){
         echo "too few arguments, function takes 2 arguments"
         exit 1
     fi
-    
-    int1_n=1
-    int2_n=1   
-    DECIMALS=0
-
-    return=$(process_floats $1 $2)
-    
-    whole_num_1=$( echo $return | cut -d'|' -f1 )
-    whole_num_2=$( echo $return | cut -d'|' -f2 )
-    int1_n=$( echo $return | cut -d'|' -f3 )
-    int2_n=$( echo $return | cut -d'|' -f4 )
-    DECIMALS=$( echo $return | cut -d'|' -f5 )
-
-    ######## both numbers are  preporcessed	
-        
-    if [[ $int1_n -lt 0 ]];then
-        (( whole_num_1 *= -1 ))
-    fi
-    if [[ $int2_n -lt 0 ]];then
-        (( whole_num_2 *= -1 ))
-    fi
 
     comma=0
     result=
-    first=$whole_num_1
-    second=$whole_num_2
     remain=0
     trail_zero=
+    DECIMALS=0
+    value=1
+    count=1
+    #just like in sum_flaot we need to now the quantity of decimals
+    #before anything else
+    for i in $@
+    do
+        decimal="${i#*","}"
+        if [[ $DECIMALS -lt ${#decimal} ]];then
+                DECIMALS=${#decimal}
+        fi
+    done
+    ##now we preprocess the numbers
+    for i in $@
+    do
+        if [[ ! $i == *","* ]];then
+            i=$i,0
+        fi
+        decimal="${i#*","}"
+        number=${i//,}
+        if [[ $number -lt 0 ]];then
+            (( value *= -1 ))
+            (( number *= -1 ))
+        fi
+        if [[ $DECIMALS -gt ${#number} ]];then
+            adjust=$(( $DECIMALS - ${#decimal} ))
+            (( number *= $(fpow 10 $adjust) ))
+        fi
+        declare number${count}=$number
+        (( count++ ))
+    done
+    #division algorithm, up to 10 steps
     for (( i=1; i<=10; i++ ))
     do 
         #"$first / $second = $result"
         #"$remain"
-        div=$(( $first / $second ))
+        div=$(( number1 / number2 )) 
         if [[ $div -eq 0 ]];then
             if [[ $comma -eq 0 ]];then
                 result="$result"0,
                 comma=1
-                (( first *= 10 ))
+                (( number1 *= 10 ))
             else
                 if [[ $remain -eq 0 ]];then
                     result="$result"0
-                    (( first *= 10 ))
+                    (( number1 *= 10 ))
                 else
-                    (( first *= 10 ))
+                    (( number1 *= 10 ))
                 fi 
                 remain=0
             fi
         else
-        remain=$div
-        first=$(( first - ( $second * div ) ))
-        result=$result$div
+            remain=$div
+            number1=$(( number1 - ( number2 * div ) ))
+            result=$result$div
         fi
-        if [[ $first -eq 0 ]];then
+        if [[ $number1 -eq 0 ]];then
             break
         fi
-
     done
-    if [[ $value -lt 0 ]];then
+    if [[ $value -eq -1 ]];then
         result=-"$result"
     fi
     echo $result
